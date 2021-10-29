a2bd757bfb6193eac75118cbff7d94e188b4ef7b
soroush bolbolabady <soroushbolbolabady@gmail.com>
2021-10-29 22:06:02 +0330

    Second changes

--- Number_Guessing.py
+++ Number_Guessing.py
@@ -9,6 +9,30 @@ random_number = random.randrange(1 , 100)
 score = 20
 is_not_win = True 
 
+def div_2(number):
+	if number % 2 == 0:
+		print("Number Divided by 2")
+	else:
+		print("Number not divid by 2")
+def div_3(number):
+	if number % 3 == 0:
+		print("Number Divided by 3")
+	else:
+		print("Number not divid by 3")
+def div_5(number):
+	if number % 5 == 0:
+		print("Number Divided by 5")
+	else:
+		print("Number not divid by 5")
+def div_7(number):
+	if number % 7 == 0:
+		print("Number Divided by 7")
+	else:
+		print("Number not divid by 7")
+
+
+	
+
 def compare_number (random_number , guess):
 	global score
 	if random_number > guess:
@@ -28,6 +52,14 @@ def compare_number (random_number , guess):
 while is_not_win:
 	guess = int(input("\nyour guess is : "))
 	compare_number(random_number , guess)
+	if score == 18 :
+		div_7(random_number)
+	elif score == 16 :
+		div_5(random_number)
+	elif score == 14 :
+		div_3(random_number)
+	elif score == 10 :
+		div_2(random_number)
 	if random_number == guess :
 		is_not_win = False
 
